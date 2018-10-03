1472cfae44b6f17ec4e90bb70cfc2d2091dc2051
Rafael Batista <rafaelb24@yahoo.com>
2018-10-03 17:04:58 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,7 @@
 def turn_count(board)
-  board.each do |count|
-    if board.each == "X" || board.each == "O"
-      count += 1
-    end
-  end
-  count
-end
-
-
-def current_player(board)
-    if count % 2 == 0
-      "X"
-    else
-      "O"
+    board.each do |count|
+      if count == "X" || count == "O"
+        tuen += 1
+      end
     end
 end
