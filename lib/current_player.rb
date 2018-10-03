7fa60dbf345056842b4b0c17ebf04c9f2e3daaf7
Rafael Batista <rafaelb24@yahoo.com>
2018-10-03 17:21:48 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,10 +8,16 @@ def turn_count(board)
     turn
 end
 
+=begin
 def current_player(board)
   if turn_count(board) % 2 == 0
     "X"
   else
     "O"
   end
+end
+=end
+
+def current_player(board)
+    player_turn = turn_count % 2 == 0 ? "X" : "O"
 end
